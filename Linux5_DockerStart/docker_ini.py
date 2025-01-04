bcb1610ae658e2433389c49b9f0f72431ff5cdc2
Gibrin <box@umcom.ru>
2025-01-04 19:53:42 +0300

    Update and rename ini to docker_ini.py

---
+++ Linux5_DockerStart/docker_ini.py
@@ -0,0 +1,8 @@
+  import datetime
+
+  def main():
+      print("Привет, это контейнер с Docker!")
+      print("Текущее время:", datetime.datetime.now())
+
+  if __name__ == "__main__":
+      main()

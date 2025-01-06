910d9e55657f05cef9f10ad3faf6ca332761cd8d
Gibrin <box@umcom.ru>
2025-01-06 12:13:10 +0300

    docker_ini.py

--- Linux5_DockerStart/docker_ini.py
+++ Linux5_DockerStart/docker_ini.py
@@ -1,8 +1,8 @@
-  import datetime
+import datetime
 
-  def main():
-      print("Привет, это контейнер с Docker!")
-      print("Текущее время:", datetime.datetime.now())
+def main():
+  print("Привет, это контейнер с Docker!")
+  print("Текущее время:", datetime.datetime.now())
 
-  if __name__ == "__main__":
-      main()
+if __name__ == "__main__":
+  main()
